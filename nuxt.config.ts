8064f6505aeca9a11717d5f8106c20bf510c87ed
Timahi <timahilesage@gmail.com>
2024-10-28 04:44:21 +0100

    refactor: move ui to src-ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  srcDir: 'src/',
+  srcDir: 'src-ui/',
   ssr: false,
   devServer: { host: process.env.TAURI_DEV_HOST || 'localhost' },
 
